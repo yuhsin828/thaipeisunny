0a810cc900133d02fa51f239aff435a92f8163b6
yuhsin828 <71396690+yuhsin828@users.noreply.github.com>
2022-07-30 03:37:26 +0800

    nav fixed top done and improve css

--- js/main.js
+++ js/main.js
@@ -6,20 +6,28 @@ function fixedNav() {
     const topNav = $('#topNav').offset().top;
     // console.log(topNav);
     $(window).scroll(function () {
-        if ($(this).scrollTop() > topNav) {          /* 滑動到nav的距離 */
+        if ($(this).scrollTop() > topNav) {  /* 滑動到nav的距離 */
             $('.navbar-brand').removeClass('d-none');
-            $('.navbar').addClass('nav_top');     /* 加上固定效果 */
+            $('.navbar').addClass('nav_top bg_yellow shadow-sm');
             $('.nav-link').removeClass('nav_btn');
-            $('#navList').removeClass('mx-auto');
+            $('#navList').removeClass('mx-auto nav_list_bg');
             $('#navList').addClass('ms-auto');
-            
+            $('nav .text_dark').hover(function () {
+                $(this).css('color', '#000000');
+            }, function () {
+                $(this).css('color', '#333333');
+            });
         } else {
             $('.navbar-brand').addClass('d-none');
-            $('.navbar').removeClass('nav_top');  /* 移除固定效果 */
+            $('.navbar').removeClass('nav_top bg_yellow shadow-sm');
             $('.nav-link').addClass('nav_btn');
-            $('#navList').addClass('mx-auto');
+            $('#navList').addClass('mx-auto nav_list_bg');
             $('#navList').removeClass('ms-auto');
-            
+            $('nav .text_dark').hover(function () {
+                $(this).css('color', '');
+            }, function () {
+                $(this).css('color', '');
+            });
         }
     });
 }
