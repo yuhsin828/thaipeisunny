3d6214f3894da97fe5935ab858a6d0580e75eb41
yuhsin828 <71396690+yuhsin828@users.noreply.github.com>
2022-07-31 05:08:50 +0800

    modify css and footer

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    fixedNav();
+    fixedNav(); /* nav固定在上方 */
 });
 
 function fixedNav() {
@@ -8,18 +8,18 @@ function fixedNav() {
     $(window).scroll(function () {
         if ($(this).scrollTop() > topNav) {  /* 滑動到nav的距離 */
             $('.navbar-brand').removeClass('d-none');
-            $('.navbar').addClass('nav_top bg_yellow shadow-sm');
+            $('.navbar').addClass('nav_top bg_lattice shadow-sm');
             $('.nav-link').removeClass('nav_btn');
             $('#navList').removeClass('mx-auto nav_list_bg');
             $('#navList').addClass('ms-auto');
             $('nav .text_dark').hover(function () {
-                $(this).css('color', '#000000');
+                $(this).css('color', '#FA760A');
             }, function () {
                 $(this).css('color', '#333333');
             });
         } else {
             $('.navbar-brand').addClass('d-none');
-            $('.navbar').removeClass('nav_top bg_yellow shadow-sm');
+            $('.navbar').removeClass('nav_top bg_lattice shadow-sm');
             $('.nav-link').addClass('nav_btn');
             $('#navList').addClass('mx-auto nav_list_bg');
             $('#navList').removeClass('ms-auto');
